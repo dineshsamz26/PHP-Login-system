e483b1b006e3776c975ed8d7a1351277f6577210
dineshsamz26 <134496058+dineshsamz26@users.noreply.github.com>
2023-08-04 23:32:29 +0530

    header and footer php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
-    <script src='main.js'></script>
-</head>
-
-<body>
     <main>
         <div class="container">
             <header
@@ -209,7 +195,3 @@
         </div>
     </div>
 </div> -->
-
-</body>
-
-</html>
